c246791f6a756f44b4b40869800d9e191c785940
Bo-vcu <bo.vancriekingeuyttendaele@student.ucll.be>
2024-05-31 14:28:12 +0200

    nick

--- PersonDetection/multiple_cams.py
+++ PersonDetection/multiple_cams.py
@@ -1,5 +1,3 @@
-from itertools import count
-import sys
 import cv2
 import numpy
 from ultralytics import YOLO
@@ -7,9 +5,9 @@ import math
 
 def get_feed(*cameras, model = YOLO('yolov8n.pt')):
     classNames = model.names   
-    out1 = cv2.VideoWriter('output_camera_1.avi', cv2.VideoWriter_fourcc(*'MJPG'), 5, (1280, 720))
-    out2 = cv2.VideoWriter('output_camera_2.avi', cv2.VideoWriter_fourcc(*'MJPG'), 5, (1280, 720))
-    out3 = cv2.VideoWriter('output_camera_3.avi', cv2.VideoWriter_fourcc(*'MJPG'), 5, (1280, 720))
+    # out1 = cv2.VideoWriter('output_camera_1.avi', cv2.VideoWriter_fourcc(*'MJPG'), 5, (1280, 720))
+    # out2 = cv2.VideoWriter('output_camera_2.avi', cv2.VideoWriter_fourcc(*'MJPG'), 5, (1280, 720))
+    # out3 = cv2.VideoWriter('output_camera_3.avi', cv2.VideoWriter_fourcc(*'MJPG'), 5, (1280, 720))
 
     
     while True:
@@ -18,15 +16,12 @@ def get_feed(*cameras, model = YOLO('yolov8n.pt')):
         img = None
         for camera in cameras:
             success, img = camera.read()
-            if i == 1:
-                # if count%(1*camera.get(cv2.CAP_PROP_FPS)) == 0:
-                    out1.write(img)
-            elif i == 2:
-                # if count%(1*camera.get(cv2.CAP_PROP_FPS)) == 0:
-                    out2.write(img)
-            elif i == 3:
-                # if count%(1*camera.get(cv2.CAP_PROP_FPS)) == 0:
-                    out3.write(img)
+            # if i == 1:
+            #     out1.write(img)
+            # elif i == 2:
+            #     out2.write(img)
+            # elif i == 3:
+            #     out3.write(img)
             results = model.predict(img, stream=True, verbose=False)
         
             for r in results:
@@ -54,7 +49,6 @@ def get_feed(*cameras, model = YOLO('yolov8n.pt')):
             i += 1   
         feed = numpy.concatenate(camera_images, axis=1)
         
-        #hieronder kan feed veranderd worden naar img om output in aparte vensters te tonen
         cv2.imshow('feed', feed)
         if cv2.waitKey(10) & 0xFF == 27:
             break
@@ -67,15 +61,15 @@ def get_feed(*cameras, model = YOLO('yolov8n.pt')):
    
 
 def get_image_from_unitree(index_camera = 1):
-    IpLastSegment = "123"
+    IpLastSegment = "162"
     udpstrPrevData = "udpsrc address=192.168.123." + IpLastSegment + " port="
     udpPORT = [9201, 9202, 9203, 9204, 9205]
     udpstrBehindData = " ! application/x-rtp,media=video,encoding-name=H264 ! rtph264depay ! h264parse ! avdec_h264 ! videoconvert ! appsink"
     udpSendIntegratedPipe = udpstrPrevData + str(udpPORT[index_camera - 1]) + udpstrBehindData
     # print("udpSendIntegratedPipe:", udpSendIntegratedPipe)
     cap = cv2.VideoCapture( udpSendIntegratedPipe ,cv2.CAP_GSTREAMER)
-    cap.set(cv2.CAP_PROP_FRAME_WIDTH, 800)
-    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 600)
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
     return cap
 
 
@@ -95,8 +89,9 @@ cap2.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
 # cap3.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
 
 
-get_feed(cap, cap2)
+# get_feed(cap, cap2)
 # get_feed(cap, cap2, cap3)
-# get_feed((get_image_from_unitree(1), get_image_from_unitree(2), get_image_from_unitree(3)))
+get_feed((get_image_from_unitree(1)))
+# , get_image_from_unitree(2), get_image_from_unitree(3)))
 
    
